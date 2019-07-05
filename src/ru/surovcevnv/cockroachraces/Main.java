c7957e678cf41e0bf6fd94990cfd435a9cc459a9
SurovcevNV <kirpix524@gmail.com>
2019-07-05 14:20:47 +0400

    start

---
+++ src/ru/surovcevnv/cockroachraces/Main.java
@@ -0,0 +1,16 @@
+package ru.surovcevnv.cockroachraces;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int tracks = Integer.parseInt(args[0]);
+//        System.out.println("Number of tracks "+tracks);
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+    }
+}
